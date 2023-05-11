04065a397e57a69c055bbab57e9af62d290133d1
courtlandia <99390213+courtlandia@users.noreply.github.com>
2023-05-11 01:47:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ st.title("D&D Character Background Generator")
 st.subheader("Too lazy to write a backstory for your D&D character? This app is for you!")
 
 # Create number inputs for each attribute
+name = st.text_area("Name", max_chars=50)
 st.header("Attributes")
 strength = st.number_input("Strength", min_value=8, max_value=15, value=8, key="strength_input")
 dexterity = st.number_input("Dexterity", min_value=8, max_value=15, value=8, key="dexterity_input")
@@ -52,7 +53,7 @@ if st.button("Create Character"):
     # Generate the D&D character using OpenAI's GPT-3 API
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=(f"A D&D player wants to create a new character in the {campaign_setting} setting. "
+        prompt=(f"A D&D player wants to create a new character named {XXXXXX} in the {campaign_setting} setting with a {background.lower()} background and {alignment.lower()} alignment. "
                 f"The character is a {sex} {race} {character_class} with the following attributes: Strength {strength}, Dexterity {dexterity}, Constitution {constitution}, Intelligence {intelligence}, Wisdom {wisdom}, Charisma {charisma}. "
                 f"The player described the character as follows: '{description}'. Based on this information, please generate a compelling backstory for this character that is 5 paragraphs long and does not include any death."),
         max_tokens=2048,
