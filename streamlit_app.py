2ef2b074d956b48b416a8f59369b6f9ade020f08
courtlandia <99390213+courtlandia@users.noreply.github.com>
2023-05-11 09:58:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,26 +16,12 @@ temperature = st.sidebar.selectbox(
 st.title("D&D Character Background Generator")
 st.subheader("Too lazy to write a backstory for your D&D character? This app is for you!")
 
-# Create number inputs for each attribute
-st.header("Attributes")
-strength = st.number_input("Strength", min_value=8, max_value=15, value=8, key="strength_input")
-dexterity = st.number_input("Dexterity", min_value=8, max_value=15, value=8, key="dexterity_input")
-constitution = st.number_input("Constitution", min_value=8, max_value=15, value=8, key="constitution_input")
-intelligence = st.number_input("Intelligence", min_value=8, max_value=15, value=8, key="intelligence_input")
-wisdom = st.number_input("Wisdom", min_value=8, max_value=15, value=8, key="wisdom_input")
-charisma = st.number_input("Charisma", min_value=8, max_value=15, value=8, key="charisma_input")
-
-# Calculate remaining points
-point_buy_cost = {8: 0, 9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 7, 15: 9}
-remaining_points = 27 - point_buy_cost[strength] - point_buy_cost[dexterity] - point_buy_cost[constitution] - point_buy_cost[intelligence] - point_buy_cost[wisdom] - point_buy_cost[charisma]
-st.write("Remaining points:", remaining_points)
-
-# Create select box inputs for character details
+# Create form inputs for character details n the sidebar
 background_options = ['Acolyte', 'Criminal', 'Guild Artisan', 'Hermit', 'Noble', 'Outlander', 'Sage', 'Soldier']
 alignment_options = ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil']
 
 with st.sidebar.form(key='my_form'):
-    st.write("Character details")
+    st.header("Character details")
     name = st.text_input("Name", max_chars=50)
     race = st.selectbox("Race", ["Human", "Elf", "Dwarf", "Halfling", "Gnome", "Half-Elf", "Half-Orc", "Warforged", "Goblin", "Hobgoblin", "Orc", "Bugbear"])
     character_class = st.selectbox("Class", ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"])
@@ -44,6 +30,20 @@ with st.sidebar.form(key='my_form'):
     background = st.selectbox('Select the character\'s background:', background_options)
     alignment = st.selectbox('Select the character\'s alignment:', alignment_options)
     
+    # Create number inputs for each attribute
+    st.header("Character attributes")
+    strength = st.number_input("Strength", min_value=8, max_value=15, value=8, key="strength_input")
+    dexterity = st.number_input("Dexterity", min_value=8, max_value=15, value=8, key="dexterity_input")
+    constitution = st.number_input("Constitution", min_value=8, max_value=15, value=8, key="constitution_input")
+    intelligence = st.number_input("Intelligence", min_value=8, max_value=15, value=8, key="intelligence_input")
+    wisdom = st.number_input("Wisdom", min_value=8, max_value=15, value=8, key="wisdom_input")
+    charisma = st.number_input("Charisma", min_value=8, max_value=15, value=8, key="charisma_input")
+
+    # Calculate remaining points
+    point_buy_cost = {8: 0, 9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 7, 15: 9}
+    remaining_points = 27 - point_buy_cost[strength] - point_buy_cost[dexterity] - point_buy_cost[constitution] - point_buy_cost[intelligence] - point_buy_cost[wisdom] - point_buy_cost[charisma]
+    st.write("Remaining points:", remaining_points)
+
     # When the user is done, they press the "Submit" button to process their inputs
     submit_button = st.form_submit_button(label='Create Character')
 
